1b1799ed6c3539350247573d4b562e7c4134ed27
demirdamla <damla.demir@cts.com.tr>
2021-08-23 16:53:32 +0300

    adde vue router history mode config for production

--- server.js
+++ server.js
@@ -3,5 +3,16 @@ const serveStatic = require("serve-static");
 const path = require("path");
 app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
+app.use(
+  history({
+    disableDotRule: true,
+    verbose: true,
+  })
+);
+
+app.get("/", function (req, res) {
+  res.render(path.join(__dirname + "/dist/index.html"));
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port);
