a7f143d3cd1e4a1c83b27ca9c3804e389a8eaab8
demirdamla <damla.demir@cts.com.tr>
2021-08-23 17:18:28 +0300

    added router history mode settings

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+var history = require('connect-history-api-fallback');
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
 app = express();
+app.use(history());    
 app.use(serveStatic(path.join(__dirname, "dist")));
 const port = process.env.PORT || 3000;
 app.listen(port);
