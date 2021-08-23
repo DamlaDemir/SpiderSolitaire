6c9589ecd4b9e7a8a929f3ecbf2ee9f7eb9d579e
demirdamla <damla.demir@cts.com.tr>
2021-08-23 16:54:06 +0300

    added vue router history config for production

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
+const history = require("connect-history-api-fallback");
+
 app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
 app.use(
