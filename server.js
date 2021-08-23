1ef247359583de1bc2864de8a0e65f12a1e3078f
demirdamla <damla.demir@cts.com.tr>
2021-08-23 17:09:01 +0300

    removed history configs in server.js

--- server.js
+++ server.js
@@ -1,20 +1,7 @@
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
-const history = require("connect-history-api-fallback");
-
 app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
-app.use(
-  history({
-    disableDotRule: true,
-    verbose: true,
-  })
-);
-
-app.get("/", function (req, res) {
-  res.render(path.join(__dirname + "/dist/index.html"));
-});
-
 const port = process.env.PORT || 3000;
 app.listen(port);
